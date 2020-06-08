78b1b6699c7e0310e13a4e5b6356763f20e3c88f
nazarmubeen <fam.nazar@gmail.com>
2020-06-08 19:55:19 +0530

    base version

---
+++ basics/src/main/java/basics/Test.java
@@ -0,0 +1,11 @@
+package basics;
+
+import org.jcp.xml.dsig.internal.SignerOutputStream;
+
+import java.util.SortedSet;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hel lo world");
+    }
+}
