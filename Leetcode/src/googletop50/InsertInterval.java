e4b6fa1b70cd7a8bd1f93e36994d09c18b58dbde
Tarun Sharma <tarun10994@gmail.com>
2020-06-14 17:10:38 +0530

    InsertInterval optimized - sorting of list removed

--- Leetcode/src/googletop50/InsertInterval.java
+++ Leetcode/src/googletop50/InsertInterval.java
@@ -6,26 +6,20 @@ import java.util.List;
 https://leetcode.com/problems/insert-interval/
  */
 public class InsertInterval {
-
-        public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> list=new ArrayList<>();
-        int index=0;
-        for(int[] curr:intervals)
-        {
-            if(curr[0]>newInterval[1] || curr[1]<newInterval[0])
-            {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> list = new ArrayList<>();
+        int index = 0;
+        for (int[] curr : intervals) {
+            if (curr[0] > newInterval[1] || curr[1] < newInterval[0]) {
                 list.add(curr);
-                if(curr[0]<newInterval[0])
+                if (curr[0] < newInterval[0])
                     index++;
-            }
-            else
-            {
-                newInterval[0]=Math.min(curr[0],newInterval[0]);
-                newInterval[1]=Math.max(curr[1],newInterval[1]);
+            } else {
+                newInterval[0] = Math.min(curr[0], newInterval[0]);
+                newInterval[1] = Math.max(curr[1], newInterval[1]);
             }
         }
-        list.add(index,newInterval);
-
+        list.add(index, newInterval);
         return list.toArray(new int[list.size()][2]);
     }
 }
